ed2a6d9fb80cc67fddf3dbf903eca086678b158e
Estela Geovanna <85539885+EstelaIb@users.noreply.github.com>
2023-01-14 20:19:26 -0500

    Add favicon for maritozzo

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="{{asset('storage\favicon.svg')}}" type="image/svg">
+    <link rel="shortcut icon" href="{{asset('storage/favicon.svg')}}" type="image/svg">
     <link href="https://cdn.jsdelivr.net/npm/daisyui@2.46.0/dist/full.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
